eab2a31fe7f633cf70b5cc3e337175211d4aa310
ammernorzan2514-maker <ammernorzan2514@gmail.com>
2025-09-25 21:55:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const startBttn = document.getElementById('startBttn');
 const start = document.getElementById('start');
 const mainPage = document.getElementById('mainPage');
 
-/*start.style.display = 'none';
-mainPage.style.display = 'block';*/
+const bgMusic = new Audio('lagu.mp3'); // letak file lagu.mp3 sama folder
+bgMusic.loop = true;     // ulang tanpa henti
+bgMusic.volume = 0.5;    // set volume tetap (0.0 – 1.0)
+
+
 startBttn.addEventListener('click', () => {
   // sembunyikan start screen
   start.style.display = 'none';
   // tunjukkan main page
   mainPage.style.display = 'block';
+   bgMusic.play().catch(err => console.log('Autoplay block:', err));
 });
 function loadPage(event, page) {
 
